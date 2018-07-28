e875516d7985935b5e2b36ac258cb5c54bc1e6fb
Dieter De Meyer <dieterdemeyer@gmail.com>
2018-07-28 19:42:57 +0200

    Change download url

--- Casks/mysides.rb
+++ Casks/mysides.rb
@@ -2,7 +2,7 @@ cask 'mysides' do
   version '1.0.1'
   sha256 '53c936f87558e174048f7d77003a0f32bbaa6a86ee52c24c52d66ffe733f0ddc'
 
-  url "https://dl.dropboxusercontent.com/s/tspl7jipr5nudpq/mysides-#{version}.dmg"
+  url "https://dl.dropboxusercontent.com/s/2hmtsxcijd1isgk/mysides-#{version}.dmg"
   name 'mysides'
   homepage 'https://github.com/mosen/mysides'
 
